14c7020a4c1fe51e623df12b141746dec89c54ce
adroffner <adroffner@gmail.com>
2015-12-10 21:44:39 -0800

    Add help docs to account.py and give its unit tests 100% code coverage.

--- abcbank/account.py
+++ abcbank/account.py
@@ -5,39 +5,68 @@ SAVINGS = 1
 MAXI_SAVINGS = 2
 
 
-class Account:
+class Account(object):
+    ''' An Account belongs to a Customer.
+    '''
+
     def __init__(self, accountType):
+        '''Create an Account.
+
+        :param int accountType: an account type, e.g. account.CHECKING
+        '''
         self.accountType = accountType
         self.transactions = []
 
     def deposit(self, amount):
+        ''' Deposit `amount` dollars into this account.
+         The `amount` is a positive dollar value taken from the customer.
+
+        :param float amount: deposit amount in USD
+        '''
         if (amount <= 0):
             raise ValueError("deposit amount must be greater than zero")
         else:
             self.transactions.append(Transaction(amount))
 
     def withdraw(self, amount):
+        ''' Withdraw `amount` dollars from this account.
+         The `amount` is a positive dollar value given to the customer.
+
+        :param float amount: withdraw amount in USD
+        '''
         if (amount <= 0):
             raise ValueError("withdraw amount must be greater than zero")
         else:
             self.transactions.append(Transaction(-amount))
 
     def interestEarned(self):
+        ''' Calculate the interest earned on all transactions.
+        This computes interest on all deposits and withdrawls during the period.
+
+        :returns: interest earned in USD
+        '''
         amount = self.sumTransactions()
         if self.accountType == SAVINGS:
             if (amount <= 1000):
                 return amount * 0.001
             else:
                 return 1 + (amount - 1000) * 0.002
-        if self.accountType == MAXI_SAVINGS:
+        elif self.accountType == MAXI_SAVINGS:
             if (amount <= 1000):
                 return amount * 0.02
             elif (amount <= 2000):
                 return 20 + (amount - 1000) * 0.05
             else:
                 return 70 + (amount - 2000) * 0.1
-        else:
+        elif self.accountType == CHECKING:
             return amount * 0.001
+        else:
+            raise ValueError("account has an invalid account type")
 
     def sumTransactions(self, checkAllTransactions=True):
+        ''' Computes all deposits and withdrawls during the period.
+
+        :returns: current monies without interest in USD
+        '''
         return sum([t.amount for t in self.transactions])
+
--- tests/account_tests.py
+++ tests/account_tests.py
@@ -7,28 +7,76 @@ from abcbank.account import Account, CHECKING, SAVINGS, MAXI_SAVINGS
 class AccountTests(TestCase):
     
     def test_deposit(self):
-        amount = 5
+        amount = 5.0
         a = Account(CHECKING)
         a.deposit(amount)
         self.assertGreaterEqual(amount, 0)
         self.assertIsInstance(a.transactions[0], Transaction, "correct type")
     
     def test_deposit_error(self):
-        amount = -5
+        amount = -5.0
         a = Account(CHECKING)
         with self.assertRaisesRegexp(ValueError, r'deposit amount must be greater than zero'):
             a.deposit(amount)
 
     def test_withdraw(self):
-        amount = 2
+        amount = 2.0
         a = Account(CHECKING)
         a.withdraw(amount)
         self.assertGreaterEqual(amount, 0)
         self.assertIsInstance(a.transactions[0], Transaction, "correct type")
     
     def test_withdraw_error(self):
-        amount = -2
+        amount = -2.0
         a = Account(CHECKING)
         with self.assertRaisesRegexp(ValueError, r'withdraw amount must be greater than zero'):
             a.withdraw(amount)
+
+    def test_interest_checking(self):
+        amount = 200.00
+        interest = amount * 0.001
+        a = Account(CHECKING)
+        a.deposit(amount)
+        iy = a.interestEarned()
+        self.assertEqual(iy, interest)
+
+    def test_interest_savings_low(self):
+        amount = 370.00
+        interest = amount * 0.001
+        a = Account(SAVINGS)
+        a.deposit(amount)
+        iy = a.interestEarned()
+        self.assertEqual(iy, interest)
+
+    def test_interest_savings_high(self):
+        amount = 1250.00
+        interest = (amount - 1000.0) * 0.002 + 1.0
+        a = Account(SAVINGS)
+        a.deposit(amount)
+        iy = a.interestEarned()
+        self.assertEqual(iy, interest)
+
+    def test_interest_maxi_savings_low(self):
+        amount = 370.00
+        interest = amount * 0.02
+        a = Account(MAXI_SAVINGS)
+        a.deposit(amount)
+        iy = a.interestEarned()
+        self.assertEqual(iy, interest)
+
+    def test_interest_maxi_savings_high(self):
+        amount = 1250.00
+        interest = (amount - 1000) * 0.05 + 20.0
+        a = Account(MAXI_SAVINGS)
+        a.deposit(amount)
+        iy = a.interestEarned()
+        self.assertEqual(iy, interest)
+
+    def test_interest_maxi_savings_max(self):
+        amount = 2350.00
+        interest = (amount - 2000) * 0.1 + 70.0
+        a = Account(MAXI_SAVINGS)
+        a.deposit(amount)
+        iy = a.interestEarned()
+        self.assertEqual(iy, interest)
 
