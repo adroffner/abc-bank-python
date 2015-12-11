7b116bf89b6f696abad06ae4c2ecd79d4597bcaf
adroffner <adroffner@gmail.com>
2015-12-10 23:12:06 -0800

    add help docs to bank.py and improve code coverage.

--- abcbank/bank.py
+++ abcbank/bank.py
@@ -1,25 +1,36 @@
-class Bank:
+class Bank(object):
+    ''' A Bank branch has one or more Customers.
+    '''
+
     def __init__(self):
+        ''' Create a bank branch.
+        '''
         self.customers = []
 
     def addCustomer(self, customer):
+        ''' Add a new customer to the branch.
+
+        :param Customer customer: add a new Customer object
+        '''
         self.customers.append(customer)
+
     def customerSummary(self):
-        summary = "Customer Summary"
-        for customer in self.customers:
-            summary = summary + "\n - " + customer.name + " (" + self._format(customer.numAccs(), "account") + ")"
-        return summary
-    def _format(self, number, word):
-        return str(number) + " " + (word if (number == 1) else word + "s")
+        ''' List a summary of customers who have accounts at this branch.
+
+        :returns: a customer summary string
+        '''
+        summary = "Customer Summary\n - "
+        return summary + "\n - ".join([ str(customer) for customer in self.customers ])
+
     def totalInterestPaid(self):
+        ''' Calculate the total interest paid by the bank during this period.
+
+        :returns: total interest in USD
+        :rtype: float
+        '''
         total = 0
         for c in self.customers:
             total += c.totalInterestEarned()
         return total
-    def getFirstCustomer(self):
-        try:
-            self.customers = None
-            return self.customers[0].name
-        except Exception as e:
-            print(e)
-            return "Error"
+
+    # def getFirstCustomer(self): There is no use for this method, and it was buggy!
